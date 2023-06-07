1f5ea0d584fc0676b495b1651d4ce374c3974c16
ismail <ismailkarbhari91@gmail.com>
2023-06-07 12:11:15 +0530

    csv category

---
+++ CsvCategory/src/Config/admin-menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    // [
+    //   'key'        => 'categorycsv',
+    //   'name'       => 'Category Upload',
+    //   'route'      => 'category.admin.index',
+    //   'sort'       => 1,
+    //   'icon-class' => 'dashboard-icon',
+    // ]
+
+    [
+      'key'        => 'catalog.categorycsv',
+      'name'       => 'Category Upload',
+      'route'      => 'category.admin.index',
+      'sort'       => 6,
+      'icon-class' => '',
+  ]
+];
